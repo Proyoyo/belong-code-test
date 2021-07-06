cb24416df3e74c91eaa51da8beeaccf071e1f9b3
Stephen Prayogo <stephenprayogo@gmail.com>
2021-07-06 17:36:27 +1000

    Refactored out updateGridWithRules.

--- src/App.tsx
+++ src/App.tsx
@@ -22,44 +22,50 @@ const NEIGHBOURS_POSITION = [
 
 const generateEmptyGrid = (): Array<Array<number>> => Array(NUMBER_OF_ROWS).fill(Array(NUMBER_OF_COLUMNS).fill(0));
 
+// Pass in 2 grids that are identical but refers to a different object
+const updateGridWithRules = (currentGrid: Array<Array<number>>, draftGrid: Array<Array<number>>): Array<Array<number>> => {
+// Go through all cells in the grid
+  for (let i = 0; i < NUMBER_OF_ROWS; i++) {
+    for (let j = 0; j < NUMBER_OF_COLUMNS; j++) {
+    // Calculate how many neighbours each cell has
+      let cellNeighbours = 0;
+      NEIGHBOURS_POSITION.forEach(([iModifier, jModifier]) => {
+        let neighbourI = i + iModifier;
+        let neighbourJ = j + jModifier;
+
+        // Make the board wraps around
+        if (neighbourI < 0) {
+          neighbourI = NUMBER_OF_ROWS - 1;
+        }
+        if (neighbourI === NUMBER_OF_ROWS) {
+          neighbourI = 0;
+        }
+        if (neighbourJ < 0) {
+          neighbourJ = NUMBER_OF_COLUMNS - 1;
+        }
+        if (neighbourJ === NUMBER_OF_COLUMNS) {
+          neighbourJ = 0;
+        }
+        cellNeighbours += currentGrid[neighbourI][neighbourJ];
+      });
+      // Rule for underpopulation & overpopulation
+      if (cellNeighbours < 2 || cellNeighbours > 3) {
+        draftGrid[i][j] = 0;
+      // If the cell is dead and there are 3 neighbours, the cell comes alive
+      } else if (currentGrid[i][j] === 0 && cellNeighbours === 3) {
+        draftGrid[i][j] = 1;
+      }
+    // Other than these 2 rules, the cell will continue to live (no change/use previous value)
+    }
+  }
+  return draftGrid;
+};
+
 const App: React.FC = () => {
   const [grid, setGrid] = useState<Array<Array<number>>>(generateEmptyGrid());
   const spawnNextGeneration = useCallback(() => {
     setGrid((currentGrid) => produce(currentGrid, (draftGrid) => {
-      // Go through all cells in the grid
-      for (let i = 0; i < NUMBER_OF_ROWS; i++) {
-        for (let j = 0; j < NUMBER_OF_COLUMNS; j++) {
-          // Calculate how many neighbours each cell has
-          let cellNeighbours = 0;
-          NEIGHBOURS_POSITION.forEach(([iModifier, jModifier]) => {
-            let neighbourI = i + iModifier;
-            let neighbourJ = j + jModifier;
-
-            // Make the board wraps around
-            if (neighbourI < 0) {
-              neighbourI = NUMBER_OF_ROWS - 1;
-            }
-            if (neighbourI === NUMBER_OF_ROWS) {
-              neighbourI = 0;
-            }
-            if (neighbourJ < 0) {
-              neighbourJ = NUMBER_OF_COLUMNS - 1;
-            }
-            if (neighbourJ === NUMBER_OF_COLUMNS) {
-              neighbourJ = 0;
-            }
-            cellNeighbours += currentGrid[neighbourI][neighbourJ];
-          });
-          // Rule for underpopulation & overpopulation
-          if (cellNeighbours < 2 || cellNeighbours > 3) {
-            draftGrid[i][j] = 0;
-            // If the cell is dead and there are 3 neighbours, the cell comes alive
-          } else if (currentGrid[i][j] === 0 && cellNeighbours === 3) {
-            draftGrid[i][j] = 1;
-          }
-          // Other than these 2 rules, the cell will continue to live (no change/use previous value)
-        }
-      }
+      updateGridWithRules(currentGrid, draftGrid);
     }));
   }, []);
   return (
